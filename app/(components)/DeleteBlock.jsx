b8802f019bcd864b01f5addff804427aeaf72e45
davidmcdougal <@dmcdou8>
2024-04-11 15:07:08 -0600

    updated fetch and post requests

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -8,7 +8,7 @@ import React from "react";
 const DeleteBlock = ({ id }) => {
   const router = useRouter();
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`${process.env.AUTHO_BASE_URL}/api/Tickets/${id}`, {
       method: "DELETE",
     });
     if (res.ok) {
