29c1624961787861d36ec79bd7e9023913e4a402
davidmcdougal <@dmcdou8>
2024-04-11 14:52:29 -0600

    completed basic ticket app

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,30 @@
+import TicketForm from "@/app/(components)/TicketForm";
 import React from "react";
 
-const TicketPage = ({ params }) => {
-  return <div>TicketPage {params.id}</div>;
+const getTicketById = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-store",
+    method: "GET",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to get ticket.");
+  }
+  return res.json();
+};
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updatedTicketData = {};
+
+  if (EDITMODE) {
+    updatedTicketData = await getTicketById(params.id);
+    updatedTicketData = updatedTicketData.foundTicketData;
+  } else {
+    updatedTicketData = {
+      _id: "new",
+    };
+  }
+  return <TicketForm ticket={updatedTicketData} />;
 };
 
 export default TicketPage;
